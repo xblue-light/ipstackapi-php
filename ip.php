5f086f4e06633c6014979c7a036114fb88933e6e
Paul Bowyer <xblue@protonmail.ch>
2019-05-14 10:54:30 +0300

    :apple: tue-may-14-testing-proxy-headers-hx

--- ip.php
+++ ip.php
@@ -5,23 +5,27 @@
 //     $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
 //     $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
 // }
-// $client  = $_SERVER['HTTP_CLIENT_IP'];
-// $forward = $_SERVER['HTTP_X_FORWARDED_FOR'];
-// $remote  = $_SERVER['REMOTE_ADDR'];
-// if(filter_var($client, FILTER_VALIDATE_IP))
-// {
-//     $ip = $client;
-// }
-// elseif(filter_var($forward, FILTER_VALIDATE_IP))
-// {
-//     $ip = $forward;
-// }
-// else
-// {
-//     $ip = $remote;
-// }
+if (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+    $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_X_FORWARDED_FOR"];
+    $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_X_FORWARDED_FOR"];
+}
+$client  = $_SERVER['HTTP_CLIENT_IP'];
+$forward = $_SERVER['HTTP_X_FORWARDED_FOR'];
+$remote  = $_SERVER['REMOTE_ADDR'];
+if(filter_var($client, FILTER_VALIDATE_IP))
+{
+    $ip = $client;
+}
+elseif(filter_var($forward, FILTER_VALIDATE_IP))
+{
+    $ip = $forward;
+}
+else
+{
+    $ip = $remote;
+}
 
-// echo $ip;
+echo $ip;
 
 // $proxy_headers = array(
 //     'HTTP_VIA',
@@ -45,9 +49,9 @@
 //     else die('Erm, doesnt seem like your using a proxy...');
 // }
 
-$server = $_SERVER;
-foreach ($server as $key => $value) {
-    //echo "{$key} => {$value} <br/>";
-}
+// $server = $_SERVER;
+// foreach ($server as $key => $value) {
+//     echo "{$key} => {$value} <br/>";
+// }
 
 ?>
